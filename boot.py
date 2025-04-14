6019eff42976911c23eb4f86fbceda6595fe0579
chris-88 <chris@l1.ie>
2025-04-14 14:14:22 +0100

    add: dns server for iOS captive auto launch setup url

--- boot.py
+++ boot.py
@@ -2,10 +2,15 @@ import network
 import time
 import machine
 import os
+import _thread
 
-from lib.config_manager import ConfigManager
+from lib import config_manager
+from lib import webserver
+from lib import dnsserver
 
-cfg = ConfigManager()
+cfg = config_manager.ConfigManager()
+
+AP_IP = "192.168.4.1"
 
 def start_access_point():
     ap = network.WLAN(network.AP_IF)
@@ -25,10 +30,10 @@ def try_connect_wifi(ssid, password, retries=10):
         print(f"Trying to connect... {retries}s left")
 
     if wlan.isconnected():
-        print('Connected to WiFi:', wlan.ifconfig())
+        print('Connected to Wi-Fi:', wlan.ifconfig())
         return True
     else:
-        print('Failed to connect to WiFi.')
+        print('Failed to connect to Wi-Fi.')
         return False
 
 def check_firmware_update(current_version):
@@ -56,10 +61,16 @@ def config_exists():
     except:
         return False
 
+def start_captive_portal():
+    print("Starting Captive Portal services...")
+    start_access_point()
+    _thread.start_new_thread(dnsserver.start_dns_server, ())
+    webserver.start_web_server()
+
 def main():
     if config_exists():
         print("Config.json found, attempting Wi-Fi connection...")
-        cfg = ConfigManager()
+        cfg = config_manager.ConfigManager()
         if try_connect_wifi(cfg.config['ssid'], cfg.config['password']):
             print("Wi-Fi connected, checking for firmware updates...")
             check_firmware_update(cfg.get_version())
@@ -68,15 +79,11 @@ def main():
             import main as app_main
             app_main.main()
         else:
-            print("Wi-Fi connection failed, starting setup AP...")
-            import lib.webserver as webserver
-            start_access_point()
-            webserver.start_web_server()
+            print("Wi-Fi connection failed, starting captive portal...")
+            start_captive_portal()
     else:
-        print("No config.json, starting setup AP...")
-        import lib.webserver as webserver
-        start_access_point()
-        webserver.start_web_server()
+        print("No config.json found, starting captive portal...")
+        start_captive_portal()
 
 if __name__ == "__main__":
     main()
