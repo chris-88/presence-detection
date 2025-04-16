9fe50d3c13922557be9294e031c74aeb6b0ca925
chris-88 <chris@l1.ie>
2025-04-16 11:55:55 +0100

    add: small change to task.py

--- lib/task.py
+++ lib/task.py
@@ -2,12 +2,12 @@ import time
 
 class Task:
     def __init__(self, interval_ms):
-        self.interval = interval_ms / 1000.0
+        self.interval_ms = interval_ms
         self.last_run = time.ticks_ms()
 
     def should_run(self):
         now = time.ticks_ms()
-        return (time.ticks_diff(now, self.last_run) >= self.interval * 1000)
+        return time.ticks_diff(now, self.last_run) >= self.interval_ms
 
     def run(self):
         raise NotImplementedError("Task must implement run()")
