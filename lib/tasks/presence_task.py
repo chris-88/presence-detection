8967649ea5503a0a541f4ad24dbded13e736032a
chris-88 <chris@l1.ie>
2025-04-19 22:15:55 +0100

    fix: change pin for PIR signal

--- lib/tasks/presence_task.py
+++ lib/tasks/presence_task.py
@@ -5,7 +5,7 @@ from lib.task import Task
 class PresenceTask(Task):
     def __init__(self, cfg, interval_ms=100):
         super().__init__(interval_ms)
-        self.pir_pin = Pin(34, Pin.IN)
+        self.pir_pin = Pin(2, Pin.IN)
         print("⏳ Waiting for PIR to stabilise...")
         time.sleep(5)  # Give PIR time to stabilise on boot
         print("✅ PIR ready.")
